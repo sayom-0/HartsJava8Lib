7f47e3f0847ec353850252f368da841c6151a446
sjet <sjet@Valkyrie-II>
2020-01-22 15:49:23 -0500

    Added protected SCFX and ebm var's to BW

--- ScreenControllerFX/src/hart/Valkyrie/util/BWindow.java
+++ ScreenControllerFX/src/hart/Valkyrie/util/BWindow.java
@@ -1,10 +1,15 @@
 package hart.Valkyrie.util;
 
+import hart.Valkyrie.SCFX.ScreenControllerFX;
+import hart.Valkyrie.objects.EventButtonManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public abstract class BWindow extends Application
 {
+	protected ScreenControllerFX SCFX;
+	protected EventButtonManager ebm;
+	
 	public void window() throws Exception
 	{
 		start(new Stage());
