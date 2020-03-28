1ec15e66c79a3fe9c322391c621d42d68168a1e8
sjet <sjet@Valkyrie-II>
2020-03-28 12:13:00 -0400

    Removed Named Array List, converted all usages to HashMap

--- ScreenControllerFX/src/hart/Valkyrie/util/DataManager.java
+++ ScreenControllerFX/src/hart/Valkyrie/util/DataManager.java
@@ -1,4 +1,5 @@
-/**Datamanager is a basic automated way of using a NAL to read and write from a file
+/**
+ * Datamanager is a basic automated way of using a NAL to read and write from a file
  *
  * @author Logan Hart
  * @version V1.1-1.16.20
@@ -12,85 +13,84 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.HashMap;
 
 import hart.Valkyrie.SuperConductor;
-import hart.Valkyrie.objects.NamedList;
 
 public class DataManager<T> implements SuperConductor
 {
-	private final static double DATAMANAGER_VERSION = 1.1;
+    private final static double DATAMANAGER_VERSION = 1.1;
 
-	private FileInputStream fis;
-	private ObjectInputStream ois;
+    private FileInputStream fis;
+    private ObjectInputStream ois;
 
-	private FileOutputStream saveos;
-	private ObjectOutputStream saveoos;
-	/** DataLine is a Public NAL that the file is copyed into */
-	public NamedList<T> DataLine;
+    private FileOutputStream saveos;
+    private ObjectOutputStream saveoos;
+    /** DataLine is a Public NAL that the file is copyed into */
+    public HashMap<String, T> DataLine;
 
-	private File iofile;
+    private File iofile;
 
-	/**
-	 * Constructor for DataManager, this links the DataManager's OIS and OOS to the
-	 * file
-	 * 
-	 * @param ifile A file object
-	 * @throws IOException
-	 */
+    /**
+     * Constructor for DataManager, this links the DataManager's OIS and OOS to the
+     * file
+     *
+     * @param file A file object
+     * @throws IOException
+     */
 
-	public DataManager(File file) throws IOException
-	{
-		iofile = file;
-		iofile.createNewFile();
-	}
+    public DataManager(File file) throws IOException
+    {
+        iofile = file;
+        iofile.createNewFile();
+    }
 
-	/**
-	 * The Load method will copy the NAL from a File into the public DataLine Field
-	 */
+    /**
+     * The Load method will copy the NAL from a File into the public DataLine Field
+     */
 
-	public void load()
-	{
-		try
-		{
-			fis = new FileInputStream(iofile);
-			ois = new ObjectInputStream(fis);
-			DataLine = (NamedList<T>) ois.readObject();
-		} catch (IOException | ClassNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-	}
+    public void load()
+    {
+        try
+        {
+            fis = new FileInputStream(iofile);
+            ois = new ObjectInputStream(fis);
+            DataLine = (HashMap<String, T>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-	/** Writes Dataline into the file */
-	public void save()
-	{
-		try
-		{
-			iofile.delete();
-			iofile.createNewFile();
-			saveos = new FileOutputStream(iofile);
-			saveoos = new ObjectOutputStream(saveos);
-			saveoos.writeObject(DataLine);
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
+    /** Writes Dataline into the file */
+    public void save()
+    {
+        try
+        {
+            iofile.delete();
+            iofile.createNewFile();
+            saveos = new FileOutputStream(iofile);
+            saveoos = new ObjectOutputStream(saveos);
+            saveoos.writeObject(DataLine);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-	public static double getDatamanagerVersion()
-	{
-		return DATAMANAGER_VERSION;
-	}
+    public static double getDatamanagerVersion()
+    {
+        return DATAMANAGER_VERSION;
+    }
 
-	@Override
-	public void deConstruct()
-	{
-		fis = null;
-		ois = null;
-		saveos = null;
-		saveoos = null;
-		DataLine.deConstruct();
-		DataLine = null;
-	}
+    @Override
+    public void deConstruct()
+    {
+        fis = null;
+        ois = null;
+        saveos = null;
+        saveoos = null;
+        DataLine = null;
+    }
 
 }
